f2fe313265e2599d2c67098d9805d2df319f297a
Lara Abu Hamad <laraabuhamad@gmail.com>
2023-03-26 16:20:58 +0300

    creating Player class

---
+++ Player.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Lara Abu Hamad on 26/03/2023.
+//
+
+#include "Player.hpp"
+Player::Player() {
+    Player::cardsLeft = 26;
+    Player::cardsWon = 0;
+}
+Player::Player(const string name) {
+    Player::name = name;
+    Player::cardsLeft = 26;
+    Player::cardsWon = 0;
+}
+
+string Player::getName(){
+    return this->name;
+}
+void Player::setName(string name){
+    this->name = name;
+}
+
+int Player::stacksize(){
+    return this->cardsLeft;
+}
+
+int Player::cardesTaken(){
+    return this->cardsWon;
+}
+
+void Player::updateCards(int numOfCards){
+    this->cardsWon += numOfCards;
+}
+
+void Player::decreaseCards(){
+    this->cardsLeft--;
+}
---
+++ Player.hpp
@@ -0,0 +1,45 @@
+//
+// Created by Lara Abu Hamad on 26/03/2023.
+//
+
+#ifndef CPP_HW2_PLAYER_H
+#define CPP_HW2_PLAYER_H
+#include <string>
+using namespace std;
+
+class Player {
+public:
+    string name;
+    int cardsLeft;
+    int cardsWon;
+public:
+    Player(){};
+    Player(const string name);
+
+    string getName(){
+        return this->name;
+    }
+    void setName(string name){
+        this->name = name;
+    }
+
+    int stacksize(){
+        return this->cardsLeft;
+    }
+
+    int cardesTaken(){
+        return this->cardsWon;
+    }
+
+    void updateCards(int numOfCards){
+        this->cardsWon = this->cardsWon + numOfCards;
+    }
+
+    void decreaseCards(){
+        this->cardsLeft--;
+    }
+
+};
+
+
+#endif //CPP_HW2_PLAYER_H

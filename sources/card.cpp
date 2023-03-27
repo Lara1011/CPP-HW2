d553b3b8f4c272145d49677012edc8cdb7eb6a0a
Lara1011 <laraabuhamad@gmail.com>
2023-03-27 17:47:15 +0300

    creating card class

---
+++ sources/card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lara Abu Hamad on 3/27/23.
+//
+
+#include "card.hpp"
---
+++ sources/card.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Lara Abu Hamad on 3/27/23.
+//
+
+#ifndef CPP_HW2_CARD_H
+#define CPP_HW2_CARD_H
+
+
+class card {
+
+};
+
+
+#endif //CPP_HW2_CARD_H

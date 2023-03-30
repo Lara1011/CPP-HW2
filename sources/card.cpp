74876336e990569b7a53ee11caf9a027343eeb7a
Lara Abu Hamad <laraabuhamad@gmail.com>
2023-03-30 12:43:08 +0200

    implementing card class

--- sources/card.cpp
+++ sources/card.cpp
@@ -2,4 +2,31 @@
 // Created by Lara Abu Hamad on 3/27/23.
 //
 
-//#include "card.hpp"
+#include "card.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace ariel{
+    Card::Card(int value, char suit): value(value), suit(suit) {
+        this->value = value;
+        this->suit = suit;
+    }
+
+    int Card::getValue() {
+        return this->value;
+    }
+
+    void Card::setValue(int val) {
+        this->value = val;
+    }
+
+    char Card::getSuit() {
+        return this->suit
+    }
+
+    void Card::setSuit(char s) {
+        this->suit = s;
+    }
+}
--- sources/card.hpp
+++ sources/card.hpp
@@ -1,18 +1,26 @@
 //
 // Created by Lara Abu Hamad on 3/27/23.
 //
-//
-//#ifndef CPP_HW2_CARD_H
-//#define CPP_HW2_CARD_H
-//
-//
-//class card {
-//private:
-//    int num;
-//    char letter;
-//public:
-//    card(){};
-//};
-//
-//
-//#endif //CPP_HW2_CARD_H
+
+#ifndef CPP_HW2_CARD_H
+#define CPP_HW2_CARD_H
+
+namespace ariel {
+    class Card {
+    private:
+        int value;
+        char suit;
+    public:
+        card(int value, char suit);
+
+        int getValue();
+
+        void setValue(int val);
+
+        char getSuit();
+
+        void setSuit(char s);
+    };
+}
+
+#endif //CPP_HW2_CARD_H
